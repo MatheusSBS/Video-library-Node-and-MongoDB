c709a14719a2b86bb554a7d4d1790ebca2ba51d4
mat <mat@mats-iMac.local>
2022-05-17 11:44:14 -0300

    conected to the database mongoose

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 const routes = require('./routes')
+
+mongoose.connect('mongodb+srv://MatheusSBS:Disquete123@cluster0.stho2.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('conected to the database!'))
+
+const app = express()
+const port = 3003
 
 app.use(routes)
 
-app.listen(3003, () => {
-    console.log('backend started')
+app.listen(port, () => {
+    console.log(`backend started at http:/localhost:${port}`)
 })
