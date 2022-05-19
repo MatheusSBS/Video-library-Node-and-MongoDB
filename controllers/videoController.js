f35723c75efa9c826a02bd3e57aabace4d91c08e
Matheus SBS <matheus.soares75@gmail.com>
2022-05-18 22:47:50 -0300

    function store to post videos  controllers/videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,14 +1,36 @@
 const { response } = require('express')
-const res = require('express')
-const Video = require('../src/models/video')
+const {v4: uuid} = require('uuid')
+const video = require('../src/models/video')
 
 module.exports = {
-    async index(req, res) {
+    async index(request, response) {
         try {
-            const videos = await Video.find()
-            return response.status(200).json({ videos })
+            const videos = await video.find()
+            return response.status(200).json({ video })
         } catch (err) {
             response.status(500).json({ error: err.message })
         }
+    },
+
+    async store(request, response) {
+        const { title, link } = request.body
+        if ( !title || !link ) {
+            return response.status(400).json({ error: "missing title or link."})
+        }
+
+        const video = new video({
+            _id: uuid(),
+            title,
+            link,
+            liked: false, 
+        })
+        try {
+            await video.save()
+
+            return response.status(201).json({ message: "Video added succesfuly!"})
+        } catch (err) {
+            response.status(400).json({ error: err.message })
+        }
+        
     }
 }
