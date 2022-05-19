19bd93a739cd7d82f3c90e4c1500e182b3e8c8bf
Matheus SBS <matheus.soares75@gmail.com>
2022-05-19 19:56:43 -0300

    routes to update with validation

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ const express = require('express')
 const routes = express.Router()
 
 const videoController = require('../controllers/videoController')
+const videoMiddleware = require('../src/middlewares/videoMiddleware')
 
 routes.get('/videos', videoController.index)
 routes.post('/videos', videoController.store)
+
+routes.put('/videos/:id', videoMiddleware.valiidateID, videoController.update)
 
 module.exports = routes
