10fae0398d4ae4fac912787d30116b3d7b02598f
Matheus SBS <matheus.soares75@gmail.com>
2022-05-19 19:55:38 -0300

    update() - put method

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -32,5 +32,24 @@ module.exports = {
             response.status(400).json({ error: err.message })
         }
         
+    },
+    
+    async update(request, response) {
+        const { title, link } = request.body
+
+        if (!title && !link) {
+            return response.status(400).json({ error: 'You must inform a new title or a new link' })
+        }
+
+        if (title) response.video.title = title
+        if (link) response.video.link = link
+
+        try {
+          await response.video.save()
+          return response.status(200).json({ message: 'Video updated successfully' })
+
+        } catch (err) {
+            response.status(500).json({ error: err.message })
+        }         
     }
 }
