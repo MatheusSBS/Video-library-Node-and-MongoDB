ac9bfd2dd22858ffd487d78fbfa85093a2f4ed40
Matheus SBS <matheus.soares75@gmail.com>
2022-05-18 22:49:32 -0300

    route to post videos

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const videoController = require('../controllers/videoController')
 const routes = express.Router()
 
-routes.get('/', (req, res) => res.send('Hello Word'))
+const videoController = require('../controllers/videoController')
+
 routes.get('/videos', videoController.index)
+routes.post('/videos', videoController.store)
 
 module.exports = routes
