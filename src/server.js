016bd8ff5d70db69d903c895092dcde09443c0b5
Matheus SBS <matheus.soares75@gmail.com>
2022-05-19 22:54:23 -0300

    cors installed

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose')
 
 const routes = require('./routes')
 const conectToDatabase = require('./database')
+const cors = require('cors')
 
 conectToDatabase()
 
 const app = express()
 const port = 3003
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
